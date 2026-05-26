174a34e2013e0720b7532cb28abf2f50832d0aed
Matthew Mateo <matthew.constante@uisel.edu.ec>
2026-05-26 08:41:40 -0500

    cambio realizado

--- pokedex/models.py
+++ pokedex/models.py
@@ -5,7 +5,6 @@ class Trainer(models.Model):
     last_name = models.CharField(max_length=100, null=False)
     birth_date = models.DateField(null=False)
     level = models.IntegerField(default=1)
-    # Foto para el entrenador
     picture = models.ImageField(upload_to="trainer_images", null=True, blank=True)
     
     def __str__(self):
@@ -22,8 +21,8 @@ class Pokemon(models.Model):
         ("Hada", "Hada"), ("Acero", "Acero"), ("Veneno", "Veneno"),
     ]
     type = models.CharField(max_length=50, choices=POKEMON_TYPES, null=False)
-    height = models.DecimalField(decimal_places=4, max_digits=10)
-    weight = models.DecimalField(decimal_places=4, max_digits=10)
+    height = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Altura (m)")
+    weight = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Peso (kg)")
     description = models.TextField(null=True, blank=True)
     trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True, related_name='pokemons')
     picture = models.ImageField(upload_to="pokemon_images", null=True, blank=True)

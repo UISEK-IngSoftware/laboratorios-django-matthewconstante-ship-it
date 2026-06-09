a3141ab70c836e38482e5738defefb390698a6a7
Matthew Mateo <matthew.constante@uisel.edu.ec>
2026-06-09 11:17:33 -0500

    finalizado?

--- lab8/urls.py
+++ lab8/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from oauth2_provider import urls as oauth2_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pokedex.urls')),  # Rutas de la web normal
-    path('api/', include('api.urls')),  # ¡Rutas de tu API de DRF!
+    path('api/', include('api.urls')), 
+    path('o/', include(oauth2_urls)),# ¡Rutas de tu API de DRF!
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
